3ffcfc20068dbc0839cb9557b1da36c3d12d5a22
harmistest <harmistest@gmail.com>
2024-01-01 17:25:32 +0530

    New Changes

--- resources/views/admin/GeneralSetting/Generalcreate.blade.php
+++ resources/views/admin/GeneralSetting/Generalcreate.blade.php
@@ -1,15 +1,16 @@
 @extends('admin.layouts.admin-layout')
-@section('title', 'Task-Management')
+@section('title', 'SETTINGS - Order Management System')
 @section('content')
+
     {{-- Page Title --}}
     <div class="pagetitle">
-        <h1>Factory Time Schedules</h1>
+        <h1>Settings</h1>
         <div class="row">
             <div class="col-md-8">
                 <nav>
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Time Schedules</li>
+                        <li class="breadcrumb-item active">settings</li>
                     </ol>
                 </nav>
             </div>
@@ -18,60 +19,58 @@
 
     {{-- Page Section --}}
     <section class="section dashboard">
-
-
         <div class="row mt-3">
             <div class="col-md-12">
                 <div class="card">
-                    <h5 class="card-title" align="center"> Time Table </h5>
                     <form method="post" action="{{ route('General.store') }}">
                         @csrf
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <div class="col-md-12 mb-3">
-                                        <div class="form-group">
-                                            <h4>Days</h4>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="form-check form-check-inline">
-                                                @foreach ($generalSettings as $generalSetting)
-                                                    <div class="form-check form-check-inline">
-                                                        <input type="text" name="Days[]" class="form-check-input" value="{{ $generalSetting->Days }}">
-                                                        <input type="checkbox" name="holiday[{{ $generalSetting->Days }}]" 
-                                                            class="form-check-input" {{ $generalSetting->holiday == 'on' ? 'checked' : '' }}>
-                                                        <label for="{{ $generalSetting->Days }}"
-                                                            class="form-check-label">{{ ucfirst($generalSetting->Days) }}</label>
-                                                    </div>
-                                                @endforeach
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <h4>Time</h4>
-                                            <input type="time" name="StartTime"
-                                                class="{{ $errors->has('StartTime') ? 'is-invalid' : '' }}"
-                                                value="{{$generalSetting->StartTime}}"> To
-                                            <input type="time" name="EndTime"
-                                                class="{{ $errors->has('EndTime') ? 'is-invalid' : '' }}"
-                                                value="{{$generalSetting->EndTime}}">
-                                            @if ($errors->has('StartTime'))
-                                                <div class="invalid-feedback">
-                                                    {{ $errors->first('StartTime') }}
+                                    <div class="form-group" style="padding-left: 1.5em;">
+                                        <label class="form-label"><strong>Working Days</strong></label>
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <div class="form-check form-check-inline">
+                                            @foreach ($generalSettings as $generalSetting)
+                                                <div class="form-check form-check-inline">
+                                                    <input type="text" name="Days[]" class="form-check-input" value="{{ $generalSetting->Days }}">
+                                                    <input type="checkbox" name="holiday[{{ $generalSetting->Days }}]" class="form-check-input" {{ $generalSetting->holiday == 'on' ? 'checked' : '' }}>
+                                                    <label for="{{ $generalSetting->Days }}" class="form-check-label">{{ ucfirst($generalSetting->Days) }}</label>
                                                 </div>
-                                            @elseif ($errors->has('EndTime'))
-                                                <div class="invalid-feedback">
-                                                    {{ $errors->first('EndTime') }}
-                                                </div>
-                                            @else
-                                            @endif
+                                            @endforeach
                                         </div>
                                     </div>
-
+                                    <div class="form-group" style="padding-left: 1.5em;">
+                                        <label class="form-label"><strong>Working Hours</strong></label>
+                                    </div>
+                                    <div class="form-group mb-3" style="padding-left: 1.5em;">
+                                        <div class="row">
+                                            <div class="col-md-2 mb-2">
+                                                <input type="time" name="StartTime" class="{{ $errors->has('StartTime') ? 'is-invalid' : '' }} form-control" value="{{$generalSetting->StartTime}}" id="from_time">
+                                                @if ($errors->has('StartTime'))
+                                                    <div class="invalid-feedback">
+                                                        {{ $errors->first('StartTime') }}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                            <div class="col-md-1 text-center">
+                                                <label class="form-label"><strong>To</strong></label>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <input type="time" name="EndTime" class="{{ $errors->has('EndTime') ? 'is-invalid' : '' }} form-control" value="{{$generalSetting->EndTime}}" id="to_time">
+                                                @if ($errors->has('EndTime'))
+                                                    <div class="invalid-feedback">
+                                                        {{ $errors->first('EndTime') }}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group" style="padding-left: 1.5em;">
+                                        <button class="btn form_button">Update</button>
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div class="card-footer text-center mt-5">
-                                <button class="btn form_button">{{ __('Save') }}</button>
                             </div>
                         </div>
                     </form>
